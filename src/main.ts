1018cded93fdb41fb037a6c55ad5146191cd8046
Andrew Plummer <plummer574@gmail.com>
2020-06-19 15:39:23 +0100

    Verbose flag (#7)

--- src/main.ts
+++ src/main.ts
@@ -17,28 +17,50 @@ interface RunProps {
     credentialsProcessOutput: boolean;
 }
 
+const getLogger = (verbose: boolean): ((msg: string) => void) => {
+    return (msg: string): void => {
+        if (verbose) {
+            console.error('INFO:', msg);
+        }
+    };
+};
+
 export const run = async (props: RunProps): Promise<void> => {
+    const log = getLogger(props.verbose);
+    log('Starting');
+
+    log('Checking CLI version...');
+
     if (!(await checkCLIVersion())) {
         throw new BadAWSCLIVersionError('Need CLI version 2, see https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html, run `aws --version` to inspect version');
     }
 
+    log('CLI version OK');
+
     if (props.credentialsProcessOutput) {
+        log('Attempting to retrieve credentials from role credentials cache file');
         const creds = await readCredentialsCacheFile();
         if (typeof creds !== 'undefined') {
+            log('Found credentials in role credentials cache file, outputting');
             printCredentials(creds);
             return;
         }
+        log('No valid credentials found in role credentials cache file, continuing');
     }
+
+    log('Locating latest SSO cache file...');
 
     let latestCacheFile = await findLatestCacheFile();
 
     if (typeof latestCacheFile === 'undefined' || latestCacheFile.expiresAt.getTime() < new Date().getTime()) {
+        log('No valid SSO cache file found, running login...');
         await execPromise('aws sso login', {
             env: {
                 ...process.env,
                 AWS_PROFILE: 'default',
             },
         });
+        log('Login completed, trying again to retrieve credentials from SSO cache');
         latestCacheFile = await findLatestCacheFile();
     }
 
@@ -46,8 +68,11 @@ export const run = async (props: RunProps): Promise<void> => {
         throw new NoCachedCredentialsError('Unable to retrieve credentials from SSO cache');
     }
 
+    log('Retrieving SSO configuration from AWS config file...');
     const ssoConfig = await findSSOConfigFromAWSConfig();
+    log('Got SSO configuration');
 
+    log('Using SSO credentials to get role credentials...');
     const getRoleCredentialsCmdOutput = await execPromise(
         `aws sso get-role-credentials --role-name "${ssoConfig.roleName}" --account-id "${ssoConfig.accountId}" --access-token "${latestCacheFile.accessToken}" --region "${latestCacheFile.region}"`,
         {
@@ -58,13 +83,25 @@ export const run = async (props: RunProps): Promise<void> => {
         },
     );
 
+    log('Parsing role credentials...');
     const roleCredentials = parseRoleCredentialsOutput(getRoleCredentialsCmdOutput.stdout);
+    log('Got role credentials');
+
     if (props.credentialsProcessOutput) {
+        log('Writing role credentials to cache in home directory...');
         await writeCredentialsCacheFile(roleCredentials);
+        log('Wrote role credentials');
+
+        log('Printing role credentials for credentials_process');
         printCredentials(roleCredentials);
+        log('Printed role credentials');
     } else {
+        log('Writing role credentials to AWS credentials file...');
         await writeCredentialsFile(roleCredentials);
+        log('Wrote role credentials');
     }
+
+    log('Done, exiting cleanly');
 };
 
 export const main = async (args: Array<string>): Promise<void> => {
