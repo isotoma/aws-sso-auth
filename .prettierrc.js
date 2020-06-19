de1f1fcfa47a1634b7c34ae54e3e9a4783701647
Andrew Plummer <plummer574@gmail.com>
2020-06-19 09:05:03 +0100

    Simpler prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,13 +3,5 @@ module.exports = {
     trailingComma: 'all',
     singleQuote: true,
     printWidth: 200,
-    tabWidth: 2,
-    overrides: [
-        {
-            files: ['*.js', '*.ts'],
-            options: {
-                tabWidth: 4,
-            },
-        },
-    ],
+    tabWidth: 4,
 };
